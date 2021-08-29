2e3337c9c865cbd3e3a178148a1455ab9a3d752d
Seok-in <hrpt8@naver.com>
2021-08-29 22:22:17 +0900

    [Add] BaekJun Algorithm

---
+++ SoftSquared/BackJun/BaekJun10171.java
@@ -0,0 +1,8 @@
+public class BaekJun10171 {
+    public static void main(String args[]){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println("\\(__)|");
+    }
+}
---
+++ SoftSquared/BackJun/BaekJun10869.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BaekJun10869 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
